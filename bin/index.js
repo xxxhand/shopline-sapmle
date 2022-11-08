33e0b81cbd570171fe605d7ad7e57767bd2c9be2
hand_wu <Hand_Wu@compal.com>
2022-11-08 13:53:37 +0800

    Declare main function

--- bin/index.js
+++ bin/index.js
@@ -1,20 +1,19 @@
 require('dotenv').config();
-
-// const { CustomMongoClient } = require('../shared/custom-mongo-client');
-
-// async function main() {
-//   await CustomMongoClient.tryOpen();
-
-// }
-
-// main().catch(ex => console.log(ex));
-
 const http = require('http');
-const { app } = require('../app');
+const { app, tryInitial } = require('../app');
 
-const _port = 9000;
-const _core = http.createServer(app.callback());
+async function main() {
+  await tryInitial();
 
-_core.listen(_port);
-_core.on('listening', () => console.log(`Server up on ${_port}`));
-_core.on('error', (err) => console.error(err.stack));
+  const _port = 9000;
+  const _core = http.createServer(app.callback());
+  
+  _core.listen(_port);
+  _core.on('listening', () => console.log(`Server up on ${_port}`));
+  _core.on('error', (err) => console.error(err.stack));
+}
+
+main().catch(ex => console.log(ex));
+
+
+
